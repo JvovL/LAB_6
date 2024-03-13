cbb2cda696c38be1110257d86140c4fdc25a41de
JvovL <jjvovo715@gmail.com>
2024-03-13 13:50:14 +0300

    Добавление фона в заметку 2-2

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -2,16 +2,27 @@ package ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
+import androidx.compose.ui.graphics.Shape
 
 @Composable fun Note(){
-    Row(modifier = Modifier.fillMaxWidth()){
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .shadow(1.dp, backgroundShape)
+        .fillMaxWidth()
+        .heightIn(min = 64.dp)
+        .background(Color.White, backgroundShape)
+    ){
     NoteColor(
         color = rwGreen,
         size = 40.dp,
