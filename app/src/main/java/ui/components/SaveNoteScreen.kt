88c78067f4f1109e2575cc0a5e9e68110cba4041
JvovL <jjvovo715@gmail.com>
2024-03-20 01:55:33 +0300

    Отображение выбранного цвета 8-1

--- app/src/main/java/ui/components/SaveNoteScreen.kt
+++ app/src/main/java/ui/components/SaveNoteScreen.kt
@@ -114,6 +114,25 @@ private fun SaveNoteTopAppBar(
     )
 }
 
+@Composable
+private fun PickedColor(color: ColorModel) {
+    Row(
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ){
+        Text(
+            text = "Picked color", modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        NoteColor(
+            color = Color.fromHex(color.hex), size = 40.dp,
+            border = 1.dp,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
 
 @Composable
 fun ColorItem(
@@ -202,4 +221,10 @@ fun SaveNoteTopAppBarPreview(){
         onOpenColorPickerClick = {},
         onDeleteNoteClick = {}
     )
+}
+
+@Preview
+@Composable
+fun PickedColorPreview(){
+    PickedColor(ColorModel.DEFAULT)
 }
